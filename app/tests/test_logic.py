03cb771be63ac5ae92890ea4ad5b44f872822df0
Alex Shmukler <alexshmukler1@gmail.com>
2017-12-10 17:28:19 +0200

    import fixed

--- app/tests/test_logic.py
+++ app/tests/test_logic.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
+import unittest
 import sys
 import os
 sys.path.append(os.environ['WORKSPACE'])
 import nodepy
-import unittest
 from selenium import webdriver
 from flask_testing import LiveServerTestCase
 from app.models import User
