754bbcbe9057c86495a09151ba00ba9af8272252
Alex Shmukler <alexshmukler1@gmail.com>
2017-12-10 17:46:57 +0200

    logic updated

--- app/tests/test_logic.py
+++ app/tests/test_logic.py
@@ -2,9 +2,6 @@ from __future__ import absolute_import
 import unittest
 from selenium import webdriver
 from flask_testing import LiveServerTestCase
-import sys
-import os
-sys.path.append(os.environ['WORKSPACE'])
 from app.models import User
 from app import app, db
 
