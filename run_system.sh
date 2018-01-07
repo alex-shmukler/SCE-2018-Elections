ec19b79f0c7b4bb76d2f0c06fb7ed58ae0d28574
dor <dordanaa@gmail.com>
2018-01-07 20:57:23 +0200

    add third selemium test

--- run_system.sh
+++ run_system.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/alex-shmukler/SCE-2018-Elections.git
-cd SCE-2018-Elections
 sudo easy_install pip
 sudo pip install -r requirements.txt
+sudo yum install git-all -y
+git clone https://github.com/alex-shmukler/SCE-2018-Elections.git
+cd SCE-2018-Elections
 python db_create.py
-sudo nohup python run.py &
+sudo nohup python run.py
